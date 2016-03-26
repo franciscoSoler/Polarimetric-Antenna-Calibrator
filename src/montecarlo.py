5dfe7a048c5f41840dc75ff703bdd01c3d802a18
Francisco <jose.francisco.tw@gmail.com>
2016-03-26 17:13:08 -0300

    fixes errors in montecarlo and the graphs were added

--- src/montecarlo.py
+++ src/montecarlo.py
@@ -1,9 +1,33 @@
 #!/usr/bin/python3.4
 import Utilities.Antenna_Common as common
+import matplotlib.pyplot as plt
 import simulator
 import numpy as np
 import json
 import sys
+import os
+
+
+def plot_results(elements, gain, phase, title, fig, filename):
+    mean_gain = np.mean(gain, axis=0).flatten()
+    mean_phase = np.mean(phase, axis=0).flatten()
+
+    plt.figure(fig)
+    plt.title(title)
+    plt.subplot(211)
+    plt.errorbar(elements, mean_gain, np.std(gain, axis=0).flatten(), fmt='ok', lw=3)
+    plt.errorbar(elements, mean_gain, [mean_gain - np.min(gain, 0).flatten(), np.max(gain, 0).flatten() - mean_gain],
+                 fmt='.k', ecolor='gray', lw=1)
+    plt.xlabel("Elementos")
+    plt.ylabel("Ganancia [dB]")
+
+    plt.subplot(212)
+    plt.errorbar(elements, mean_phase, np.std(phase, axis=0).flatten(), fmt='ok', lw=3)
+    plt.errorbar(elements, mean_phase, [mean_phase - np.min(phase, 0).flatten(), np.max(phase, 0).flatten() - mean_phase],
+                 fmt='.k', ecolor='gray', lw=1)
+    plt.xlabel("Elementos")
+    plt.ylabel("Fase [deg]")
+    plt.savefig(os.path.join('/media/francisco/Datos/francisco/Documents/mutual/written/thesis/gfx', filename + ".png"), bbox_inches='tight')
 
 
 def run_simulator(simm, mut_gain, mut_phase, clas_gain, clas_phase):
@@ -46,9 +70,15 @@ def create_config_file(errors):
     clas_phase = []
     sim = simulator.Simulator()
 
-    [run_simulator(sim, mut_gain, mut_phase, clas_gain, clas_phase) for _ in range(100)]
-    print("mutual", np.std(mut_gain, axis=0), np.std(mut_phase, axis=0))
-    print("classical", np.std(clas_gain, axis=0), np.std(clas_phase, axis=0))
+    [run_simulator(sim, mut_gain, mut_phase, clas_gain, clas_phase) for _ in range(500)]
+
+    plt.close('all')
+    elements = range(len(mut_gain[0])*len(mut_gain[0][0]))
+
+    plot_results(elements, mut_gain, mut_phase, "Calibración con acoplamientos mutuos", 1, "mutualMontecarlo")
+    plot_results(elements, clas_gain, clas_phase, "Calibración clásica", 2, "classicalMontecarlo")
+
+    plt.show()
 
 
 def main():
