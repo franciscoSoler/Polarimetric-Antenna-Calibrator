109cb5442e40feb9a9683af0c62ac76897d2805c
Francisco <jose.francisco.tw@gmail.com>
2017-07-02 12:06:00 -0300

    removed unnecessary files

--- src/readCharacterization.m
+++
@@ -1,6 +0,0 @@
-function [X, Y] = readCharacterization( qttyAnt )
-	% in this system in particular
-	qttyLines = 2*qttyAnt + 4;
-	[X(:,1), X(:,2)] = textread('RFDNcharacterization', '%f %f', qttyLines);
-	[Y(:,1), Y(:,2)] = textread('RMcharacterization', '%f %f', qttyAnt);
-end
